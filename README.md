a426d5221544d9a4afa75cd6af4eaf7f58f59fe1
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-02-01 02:06:32 +0200

    Create project files

---
+++ README.md
@@ -0,0 +1,25 @@
+# Basic Blog
+
+A full stack Node & React Blog application.
+
+## App Description
+
+- App is divided into an API backend, a visitors frontend, and an admin frontend
+- All visitors can view posts and leave comments
+- Admin can login to a seperate frontend and manage the blog
+- All CRUD operations for posts and comments are available for admin
+
+## Technical Description
+
+Backend built using:
+
+- Node.js
+- Express
+- MongoDB
+- Passport.js (authentication with JWTs)
+
+Frontends built using:
+
+- Vite
+- React
+- Tailwind
---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+import indexRouter from './routes/index.js';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+export default app;
