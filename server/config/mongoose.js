00e9d9f3de3041b5b408b7733b465c87af769b20
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-02-01 03:05:41 +0200

    Add mongoose configuration

---
+++ server/config/mongoose.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import debug from 'debug';
+import 'dotenv/config';
+
+const dbDebugger = debug('basic-blog:db');
+
+mongoose.set('strictQuery', false);
+
+const dev_db_url = 'mongodb://127.0.0.1:27017/basic-blog';
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+async function main() {
+  await mongoose.connect(mongoDB);
+  dbDebugger('DB connected');
+}
+
+main().catch((err) => debug(err));
+
+const db = mongoose.connection;
+db.on('error', debug('Mongo connection error'));
+
+export default db;
