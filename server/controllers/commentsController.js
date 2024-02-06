19588eb33f1d6849f3074090a778c9e633575ec7
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-02-06 19:57:26 +0200

    Implement comment update controller

--- server/controllers/commentsController.js
+++ server/controllers/commentsController.js
@@ -112,11 +112,86 @@ export const comment_detail = asyncHandler(async function (req, res) {
 });
 
 /* Update specific comment on PUT */
-export const comment_update = asyncHandler(async function (req, res) {
-  res.json({
-    msg: `Update comment ${req.params.id}`,
-  });
-});
+export const comment_update = [
+  body('post')
+    .optional()
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Post must contain at least 1 charcters')
+    .custom(async (val) => {
+      const foundPost = await Post.findById(val).exec();
+      if (!foundPost) {
+        throw Error();
+      }
+    })
+    .withMessage('Post is not recognized')
+    .escape(),
+  body('author', 'Author must contain at least 1 charcters')
+    .optional()
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('content', 'Content must contain at least 1 charcters')
+    .optional()
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('createdAt', 'Created At date format is invalid')
+    .optional()
+    .isISO8601()
+    .toDate(),
+  body('editedAt')
+    .optional()
+    .isISO8601()
+    .withMessage('Edited At date format is invalid')
+    .custom(async (val, { req }) => {
+      // check that editedAt is after createdAt
+      if (req.body.createdAt) {
+        if (Date.parse(val) < Date.parse(req.body.createdAt)) {
+          throw new Error();
+        }
+      } else {
+        const comment = await Comment.findById(req.params.id);
+        if (Date.parse(val) < Date.parse(comment.createdAt)) {
+          throw new Error();
+        }
+      }
+    })
+    .withMessage("Edited At can't be before Created At")
+    .toDate(),
+  asyncHandler(async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors,
+      });
+    }
+
+    let comment;
+    try {
+      comment = await Comment.findById(req.params.id).exec();
+    } catch (err) {
+      comment = null;
+    }
+
+    if (!comment) {
+      return res.status(404).json({
+        error: 'Comment not found',
+      });
+    }
+
+    Object.keys(req.body).forEach((e) => {
+      comment[e] = req.body[e];
+    });
+
+    await comment.save();
+
+    res.json({
+      success: true,
+      comment,
+    });
+  }),
+];
 
 /* Delete specific comment on DELETE */
 export const comment_delete = asyncHandler(async function (req, res) {
