9be7f97b244cd622cd8b4743b27fd122a9548a5f
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-02-08 20:20:10 +0200

    Add tailwind and react-router

--- public-client/src/main.jsx
+++ public-client/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './routes/routes';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={createBrowserRouter(routes)} />
   </React.StrictMode>
 );
