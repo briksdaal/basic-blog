594dec002cafa809e690b6acd23532d1b48c9f07
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-02-14 01:45:07 +0200

    Add timeToRead virtual field to post model

--- server/models/post.js
+++ server/models/post.js
@@ -4,15 +4,22 @@ const Schema = mongoose.Schema;
 
 mongoose.Schema.Types.String.checkRequired((v) => v != null);
 
-const PostSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  createdAt: { type: Date, required: true, default: Date.now },
-  editedAt: { type: Date, required: true, default: Date.now },
-  published: { type: Boolean, required: true, default: true },
-  commentsCount: { type: Number, required: true, default: 0 },
-  image: { type: String },
+const PostSchema = new Schema(
+  {
+    author: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    createdAt: { type: Date, required: true, default: Date.now },
+    editedAt: { type: Date, required: true, default: Date.now },
+    published: { type: Boolean, required: true, default: true },
+    commentsCount: { type: Number, required: true, default: 0 },
+    image: { type: String },
+  },
+  { toJSON: { virtuals: true } }
+);
+
+PostSchema.virtual('timeToRead').get(function () {
+  return Math.ceil(this.content.split(' ').length / 182);
 });
 
 const PostModel = mongoose.model('Post', PostSchema);
