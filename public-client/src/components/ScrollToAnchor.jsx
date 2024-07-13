668b0f8349a8e05102aea83823f0ccba4e24c474
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-13 22:08:02 +0300

    Create ScrollToAnchor component for anchor links

---
+++ public-client/src/components/ScrollToAnchor.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function ScrollToAnchor() {
+  const { hash } = useLocation();
+  useEffect(() => {
+    if (!hash) return;
+    setTimeout(() => {
+      document
+        .querySelector(hash)
+        ?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }, 500);
+  }, []);
+
+  return null;
+}
+
+export default ScrollToAnchor;
