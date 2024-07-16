d72c1df354ed6cb3b025d21c70749ff9a5fe8894
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-17 02:12:33 +0300

    Rebuild author component
    Still needs styling

--- public-client/src/components/Author.jsx
+++ public-client/src/components/Author.jsx
@@ -1,5 +1,7 @@
 import PostList from './PostList';
 import FetchWrapper from './FetchWrapper';
+import { FaRegEnvelope } from 'react-icons/fa6';
+import userPlaceholder from '/user_placeholder.svg';
 
 function Author({ data = null }) {
   const author = data?.user;
@@ -14,15 +16,29 @@ function Author({ data = null }) {
   );
 
   return (
-    <div>
-      <div>
-        {author.handle} {author.email}
+    <div className="flex flex-col md:flex-row">
+      <div className="flex-1 md:flex-initial">
+        <div className="relative flex h-48 w-48 items-center justify-center overflow-hidden rounded-full ">
+          <div className="absolute left-0 top-0 h-full w-full rounded-full border-4 border-slate-800"></div>
+          <img
+            className="h-full w-full"
+            src={
+              author.imageUrl
+                ? `${import.meta.env.VITE_API_URL}${author.imageUrl}`
+                : userPlaceholder
+            }
+            alt={author.imageUrl ? author.fullname : 'user placeholder'}
+          />
+        </div>
+        <div>{author.fullname}</div>
+        <div>@{author.handle}</div>
+        <div className="flex items-center gap-1">
+          <FaRegEnvelope /> {author.email}
+        </div>
       </div>
-      <div>
-        {author.firstname} {author.lastname}
+      <div className="flex-[3_3_0%] px-8 md:flex-initial">
+        <FetchedPostList />
       </div>
-      <h2 className="text-2xl">Posts by {author.handle}</h2>
-      <FetchedPostList />
     </div>
   );
 }
