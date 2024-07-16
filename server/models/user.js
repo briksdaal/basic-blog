cd2d2e046161eb2b7f258d8e1bf8b76a481222ae
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-16 19:05:53 +0300

    Add imageUrl virtual field to user model

--- server/models/user.js
+++ server/models/user.js
@@ -2,13 +2,27 @@ import mongoose, { mongo } from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  email: { type: String, required: true },
-  handle: { type: String, required: true },
-  password: { type: String, required: true },
-  image: { type: String },
+const UserSchema = new Schema(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    handle: { type: String, required: true },
+    password: { type: String, required: true },
+    image: { type: String },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret.image;
+      },
+    },
+  }
+);
+
+UserSchema.virtual('imageUrl').get(function () {
+  return this.image && this.image.split('public/')[1];
 });
 
 const UserModel = mongoose.model('User', UserSchema);
