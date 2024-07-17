37767afa674dccf8e5206fa0b84385f2af78af7c
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-17 03:44:26 +0300

    Update helmet page title after loading

--- public-client/src/components/Author.jsx
+++ public-client/src/components/Author.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import PostList from './PostList';
 import FetchWrapper from './FetchWrapper';
 import { FaRegEnvelope } from 'react-icons/fa6';
@@ -11,34 +12,39 @@ function Author({ data = null }) {
   }
 
   return (
-    <div className="flex flex-col md:flex-row">
-      <div className="flex-1 md:flex-initial">
-        <div className="relative flex h-48 w-48 items-center justify-center overflow-hidden rounded-full ">
-          <div className="absolute left-0 top-0 h-full w-full rounded-full border-4 border-slate-800"></div>
-          <img
-            className="h-full w-full"
-            src={
-              author.imageUrl
-                ? `${import.meta.env.VITE_API_URL}${author.imageUrl}`
-                : userPlaceholder
-            }
-            alt={author.imageUrl ? author.fullname : 'user placeholder'}
+    <>
+      <Helmet>
+        <title>JourneyJot - {author.fullname}</title>
+      </Helmet>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-1 md:flex-initial">
+          <div className="relative flex h-48 w-48 items-center justify-center overflow-hidden rounded-full ">
+            <div className="absolute left-0 top-0 h-full w-full rounded-full border-4 border-slate-800"></div>
+            <img
+              className="h-full w-full"
+              src={
+                author.imageUrl
+                  ? `${import.meta.env.VITE_API_URL}${author.imageUrl}`
+                  : userPlaceholder
+              }
+              alt={author.imageUrl ? author.fullname : 'user placeholder'}
+            />
+          </div>
+          <div>{author.fullname}</div>
+          <div>@{author.handle}</div>
+          <div className="flex items-center gap-1">
+            <FaRegEnvelope /> {author.email}
+          </div>
+        </div>
+        <div className="flex-[3_3_0%] px-8 md:flex-initial">
+          <FetchWrapper
+            Child={PostList}
+            suffixUrl={`posts/?authorid=${author._id}`}
+            minimized={true}
           />
         </div>
-        <div>{author.fullname}</div>
-        <div>@{author.handle}</div>
-        <div className="flex items-center gap-1">
-          <FaRegEnvelope /> {author.email}
-        </div>
       </div>
-      <div className="flex-[3_3_0%] px-8 md:flex-initial">
-        <FetchWrapper
-          Child={PostList}
-          suffixUrl={`posts/?authorid=${author._id}`}
-          minimized={true}
-        />
-      </div>
-    </div>
+    </>
   );
 }
 
--- public-client/src/components/SinglePost.jsx
+++ public-client/src/components/SinglePost.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import CommentsSection from './CommentSection';
 
@@ -13,29 +14,34 @@ function SinglePost({ data = null }) {
   }
 
   return (
-    <div>
-      <h2>{post.title}</h2>
-      <div className="flex gap-16">
-        {post.timeToRead} {post.timeToRead == 1 ? 'minute' : 'minutes'} read
-      </div>
-      <div className="flex gap-16">
-        <span>
-          by{' '}
-          <Link to={`/authors/${post.author._id}`}>{post.author.handle}</Link>
-        </span>
-        <span>{post.commentsCount} comments</span>
-      </div>
+    <>
+      <Helmet>
+        <title>JourneyJot - {post.title}</title>
+      </Helmet>
+      <div>
+        <h2>{post.title}</h2>
+        <div className="flex gap-16">
+          {post.timeToRead} {post.timeToRead == 1 ? 'minute' : 'minutes'} read
+        </div>
+        <div className="flex gap-16">
+          <span>
+            by{' '}
+            <Link to={`/authors/${post.author._id}`}>{post.author.handle}</Link>
+          </span>
+          <span>{post.commentsCount} comments</span>
+        </div>
 
-      {post.imageUrl && (
-        <img
-          src={`${import.meta.env.VITE_API_URL}${post.imageUrl}`}
-          alt={post.title}
-        />
-      )}
-      <p className="whitespace-break-spaces">{post.content}</p>
+        {post.imageUrl && (
+          <img
+            src={`${import.meta.env.VITE_API_URL}${post.imageUrl}`}
+            alt={post.title}
+          />
+        )}
+        <p className="whitespace-break-spaces">{post.content}</p>
 
-      <CommentsSection postId={post._id} />
-    </div>
+        <CommentsSection postId={post._id} />
+      </div>
+    </>
   );
 }
 
