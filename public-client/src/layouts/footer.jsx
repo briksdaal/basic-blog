824b53ddf823f1dc822029c1ca9f02bef0827b81
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-20 21:55:57 +0300

    Add margin top to footer

--- public-client/src/layouts/footer.jsx
+++ public-client/src/layouts/footer.jsx
@@ -2,7 +2,7 @@ import { FaGithub } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <footer className="container mx-auto flex justify-center py-4">
+    <footer className="container mx-auto mt-10 flex justify-center py-4">
       <div className="flex items-center gap-1">
         Copyright © {new Date().getFullYear()} Briksdaal{' '}
         <a
