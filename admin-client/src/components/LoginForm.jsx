c3f0c54b3f89e28f3f2466aa04a04d5679dfe4d1
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-24 02:21:34 +0300

    Implement login fetch functionality and states

--- admin-client/src/components/LoginForm.jsx
+++ admin-client/src/components/LoginForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Typography from './Typography';
 
 function Input({ type, id, label }) {
@@ -21,6 +22,10 @@ function Input({ type, id, label }) {
 }
 
 function LoginForm() {
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
   const inputs = [
     {
       type: 'email',
@@ -37,9 +42,32 @@ function LoginForm() {
   function handleSubmit(e) {
     e.preventDefault();
 
-    const form = e.target;
-    console.log(form.checkValidity());
-    console.log('submitted');
+    setErrorMsg('');
+    setSuccess(false);
+    setLoading(true);
+    const data = Object.fromEntries(new FormData(e.target));
+
+    fetch(`${import.meta.env.VITE_API_URL}/auth`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+      .then(async (res) => {
+        if (res.status > 200) {
+          const json = await res.json();
+          throw new Error(json.errors[0].msg);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setSuccess(true);
+
+        console.log(data);
+      })
+      .catch((err) => {
+        setErrorMsg(err.message);
+      })
+      .finally(() => setLoading(false));
   }
 
   return (
@@ -51,12 +79,26 @@ function LoginForm() {
         {inputs.map((i) => (
           <Input key={i.id} {...i} />
         ))}
-        <div>
-          <button
-            type="submit"
-            className="bg-mantis-400 hover:bg-mantis-300 disabled:bg-mantis-300 focus-visible:outline-mantis-400 text-md rounded-md px-6 py-2 font-bold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
-            Login
-          </button>
+        <div className="flex flex-col gap-2">
+          <div>
+            <button
+              type="submit"
+              disabled={loading}
+              className="bg-mantis-400 hover:bg-mantis-300 disabled:bg-mantis-300 focus-visible:outline-mantis-400 text-md rounded-md px-6 py-2 font-bold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+              Login
+            </button>
+          </div>
+          {errorMsg && (
+            <Typography type="small" className="text-red-600">
+              {errorMsg}
+            </Typography>
+          )}
+          {success && (
+            <Typography type="small">
+              Successfully logged in. You're being redirected to the admin
+              panel...
+            </Typography>
+          )}
         </div>
       </form>
     </div>
