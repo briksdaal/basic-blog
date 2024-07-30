b90c340c6aeca7c6f68abb4af8108a276f09cda9
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-30 18:17:47 +0300

    Add back to dashboard link on unauthorized page

--- admin-client/src/views/unauthorized-view.jsx
+++ admin-client/src/views/unauthorized-view.jsx
@@ -1,7 +1,17 @@
+import { Link, useNavigate } from 'react-router-dom';
 import Typography from '../components/General/Typography';
 
 function UnauthorizedView() {
-  return <Typography>You're not authorized to view this page</Typography>;
+  const Navigate = useNavigate();
+
+  return (
+    <div>
+      <Typography>You're not authorized to view this page</Typography>
+      <Link to="/">
+        <Typography type="link">Go back to dashboard</Typography>
+      </Link>
+    </div>
+  );
 }
 
 export default UnauthorizedView;
