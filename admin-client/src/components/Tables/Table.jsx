8619874624e6378e4480d72f64bc0b1cd670f401
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-31 05:06:10 +0300

    Overflow table on mobile

--- admin-client/src/components/Tables/Table.jsx
+++ admin-client/src/components/Tables/Table.jsx
@@ -5,52 +5,53 @@ function Table({ data, columns, route, header = true }) {
   const cellClassList = 'px-4 py-1 border-solid border-y border-slate-300';
 
   return (
-    <table className="w-full text-left">
-      {header && (
-        <thead>
-          <tr className="bg-mantis-100">
-            {columns.map((c) => (
-              <th key={c.title} scope="col" className={cellClassList}>
-                <div className={c.className}>
-                  <Typography type="smaller">{c.title}</Typography>
-                </div>
-              </th>
-            ))}
-          </tr>
-        </thead>
-      )}
-      <tbody>
-        {data.map((d) => (
-          <tr key={d._id} className="even:bg-mantis-100">
-            {columns.map((c, i) => {
-              if (i == 0) {
+    <div className="overflow-x-auto">
+      <table className="w-full text-left">
+        {header && (
+          <thead>
+            <tr className="bg-mantis-100">
+              {columns.map((c) => (
+                <th key={c.title} scope="col" className={cellClassList}>
+                  <div className={c.className}>
+                    <Typography type="smaller">{c.title}</Typography>
+                  </div>
+                </th>
+              ))}
+            </tr>
+          </thead>
+        )}
+        <tbody>
+          {data.map((d) => (
+            <tr key={d._id} className="even:bg-mantis-100">
+              {columns.map((c, i) => {
+                if (i == 0) {
+                  return (
+                    <th key={c.field} scope="row" className={cellClassList}>
+                      <div className={c.className}>
+                        <Typography type="smallerLink">
+                          <Link to={d.link || `${route}/${d._id}`}>
+                            {d[c.field]}{' '}
+                          </Link>
+                        </Typography>
+                      </div>
+                    </th>
+                  );
+                }
                 return (
-                  <th key={c.field} scope="row" className={cellClassList}>
+                  <td key={c.field} className={cellClassList}>
                     <div className={c.className}>
-                      <Typography type="smallerLink">
-                        <Link to={d.link || `${route}/${d._id}`}>
-                          {d[c.field]}{' '}
-                        </Link>
+                      <Typography type={c.typographyType || 'smaller'}>
+                        {c.fn ? c.fn(d) : d[c.field]}
                       </Typography>
                     </div>
-                  </th>
+                  </td>
                 );
-              }
-
-              return (
-                <td key={c.field} className={cellClassList}>
-                  <div className={c.className}>
-                    <Typography type={c.typographyType || 'smaller'}>
-                      {c.fn ? c.fn(d) : d[c.field]}
-                    </Typography>
-                  </div>
-                </td>
-              );
-            })}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
