be418bba2ff8fe1936075c2c1929725bc0760dbb
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-07-31 03:27:08 +0300

    Create dynamically generated table from props

---
+++ admin-client/src/components/Tables/Table.jsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+
+function Table({ data, columns, route, header = true }) {
+  const cellClassList = 'pr-8';
+
+  return (
+    <table className="text-left">
+      {header && (
+        <thead>
+          <tr>
+            {columns.map((c) => (
+              <th
+                key={c.title}
+                scope="col"
+                className={`${cellClassList} ${c.className}`}>
+                {c.title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+      )}
+      <tbody>
+        {data.map((d) => (
+          <tr key={d._id}>
+            {columns.map((c, i) => {
+              if (i == 0) {
+                return (
+                  <th
+                    key={c.field}
+                    scope="row"
+                    className={`${cellClassList} ${c.className}`}>
+                    <Link to={`${route}/${d._id}`}>{d[c.field]}</Link>
+                  </th>
+                );
+              }
+
+              return (
+                <td key={c.field} className={`${cellClassList} ${c.className}`}>
+                  {c.fn ? c.fn(d) : d[c.field]}
+                </td>
+              );
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
