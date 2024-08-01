06ee282c9ef2e3772f02e5d3e9fb6d094e89076b
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-08-01 03:53:12 +0300

    Convert login form to use rhf

--- admin-client/src/components/LoginForm.jsx
+++ admin-client/src/components/LoginForm.jsx
@@ -1,7 +1,8 @@
 import Typography from './General/Typography';
 import useLogin from '../hooks/useLogin.jsx';
+import { useForm } from 'react-hook-form';
 
-function Input({ type, id, label }) {
+function Input({ type, id, label, validations, register, errorMsg }) {
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={id}>
@@ -9,13 +10,19 @@ function Input({ type, id, label }) {
       </label>
       <div className="flex rounded-md ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-gray-600 sm:max-w-sm">
         <input
-          required
           type={type}
           id={id}
-          name={id}
+          {...register(id, { ...validations })}
           autoComplete="on"
           className="flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 outline-none focus:ring-0 sm:leading-6"
         />
+      </div>
+      <div>
+        {errorMsg && (
+          <Typography type="small" className="text-red-600">
+            {errorMsg}
+          </Typography>
+        )}
       </div>
     </div>
   );
@@ -25,34 +32,39 @@ const inputs = [
   {
     type: 'email',
     id: 'email',
-    label: 'Email:'
+    label: 'Email:',
+    validations: { required: 'Email must not be empty' }
   },
   {
     type: 'password',
     id: 'password',
-    label: 'Password:'
+    label: 'Password:',
+    validations: { required: 'Password must not be empty' }
   }
 ];
 
 function LoginForm() {
   const [login, loading, errorMsg, success] = useLogin();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    const data = Object.fromEntries(new FormData(e.target));
-
+  function onSubmit(data) {
     login(data);
   }
 
   return (
     <div className="mx-auto">
-      <form
-        method="POST"
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
         {inputs.map((i) => (
-          <Input key={i.id} {...i} />
+          <Input
+            key={i.id}
+            {...i}
+            register={register}
+            errorMsg={errors[i.id]?.message}
+          />
         ))}
         <div className="flex flex-col gap-2">
           <div>
