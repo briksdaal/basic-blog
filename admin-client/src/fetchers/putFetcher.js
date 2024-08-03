cdd328601ba39ddab9c5987da05f8f3d65756be4
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-08-03 03:17:11 +0300

    Use FormData and prepare for images in put fetcher

--- admin-client/src/fetchers/putFetcher.js
+++ admin-client/src/fetchers/putFetcher.js
@@ -1,12 +1,21 @@
 function putFetcher(fullUrl, token, payload) {
+  const formData = new FormData();
+
+  Object.entries(payload).forEach((e) => {
+    if (e[0] === 'image' && e[0].length) {
+      formData.append(e[0], payload.removeImage ? '' : e[1][0]);
+    } else {
+      formData.append(e[0], e[1]);
+    }
+  });
+
   return fetch(fullUrl, {
     method: 'PUT',
     headers: {
-      'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
       credentials: 'include'
     },
-    body: JSON.stringify(payload)
+    body: formData
   });
 }
 
