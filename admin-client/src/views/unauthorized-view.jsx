3bf3609b2d915e23d403c6abdf9c5fa3e762a022
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-08-08 02:58:58 +0300

    Always redirect admin from unauthorized to home

--- admin-client/src/views/unauthorized-view.jsx
+++ admin-client/src/views/unauthorized-view.jsx
@@ -1,7 +1,14 @@
 import PageTitle from '../components/General/PageTitle';
 import GoBack from '../components/General/GoBack';
+import useAuth from '../hooks/useAuth';
+import { Navigate } from 'react-router-dom';
 
 function UnauthorizedView() {
+  const { auth } = useAuth();
+
+  if (auth.admin) {
+    return <Navigate to="/dashboard" />;
+  }
   return (
     <div>
       <PageTitle>You're not authorized to view this page</PageTitle>
