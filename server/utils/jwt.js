1f4bccfb10e64de839f3b1a0f8b1db8fc0ffc65b
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-08-08 03:17:36 +0300

    Update jwt config

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,18 +1,30 @@
 import jwt from 'jsonwebtoken';
 
-function createToken(username, admin, key, expiresIn) {
-  return jwt.sign({ sub: username, admin }, key, {
+function createToken(username, admin, id, key, expiresIn) {
+  return jwt.sign({ sub: username, admin, id }, key, {
     expiresIn,
     algorithm: 'RS256',
   });
 }
 
-export function createRefreshToken(username, admin) {
-  return createToken(username, admin, process.env.REFRESH_PRIVATE_KEY, '1d');
+export function createRefreshToken(username, admin, id) {
+  return createToken(
+    username,
+    admin,
+    id,
+    process.env.REFRESH_PRIVATE_KEY,
+    '1d'
+  );
 }
 
-export function createAccessToken(username, admin) {
-  return createToken(username, admin, process.env.ACCESS_PRIVATE_KEY, '5m');
+export function createAccessToken(username, admin, id) {
+  return createToken(
+    username,
+    admin,
+    id,
+    process.env.ACCESS_PRIVATE_KEY,
+    '15m'
+  );
 }
 
 export function verifyRefreshToken(token) {
